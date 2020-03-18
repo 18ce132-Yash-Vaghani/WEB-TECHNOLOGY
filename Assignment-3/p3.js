63b9c8ffddeaecc257d82f1b93b429ca4d7a5907
18ce132-Yash-Vaghani <62292067+18ce132-Yash-Vaghani@users.noreply.github.com>
2020-03-18 19:31:59 +0530

    Add files via upload

---
+++ Assignment-3/p3.js
@@ -0,0 +1,7 @@
+function msg(){  
+ alert("Welcome to my page!");  
+}  
+msg();
+function changeBodyBg(color){
+        document.body.style.background = color;
+    }
---
+++ Assignment-3/p51.js
@@ -0,0 +1,13 @@
+function myFun() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
+function myFun1() {
+  var y = document.getElementById("Email");
+  y.value = y.value.toLowerCase();
+}
+
+function msg(){  
+ alert("form details are successfully submitted");  
+} 
+
